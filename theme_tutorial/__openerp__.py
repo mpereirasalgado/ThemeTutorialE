28ed94b2cffd631e6497487a21a2d6886a00ae78
Micael <micael11rm@ourlook.com>
2016-03-22 17:59:48 +0100

    Primer commit: estructura del tema hecha y subida al odoo

---
+++ theme_tutorial/__openerp__.py
@@ -0,0 +1,11 @@
+{
+    'name':'Tutorial Theme con Urcera',
+    'description': 'lod cojonrd.',
+    'category': 'Theme/Creative',
+    'sequence': 1000,
+    'version': '1.0',
+    'author': 'MIca.',
+    'depends': ['website'],
+    'data': [
+    ],
+}
